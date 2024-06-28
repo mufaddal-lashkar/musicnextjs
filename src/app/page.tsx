e6cdbec1aa1230748351c8aadbcd5c9553400246
mufaddal lashkar <mufaddal.github@gmail.com>
2024-06-28 16:09:49 +0530

    webinar section done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 import MusicSchoolTestimonial from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 
@@ -11,6 +12,7 @@ export default function Home() {
             <FeaturedCourses />
             <WhyChooseUs />
             <MusicSchoolTestimonial />
+            <UpcomingWebinars />
         </main>
     );
 }
