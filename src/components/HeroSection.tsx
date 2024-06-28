1f47de16cc4d9327f065490fb7f2da94ca0e572c
mufaddal lashkar <mufaddal.github@gmail.com>
2024-06-28 15:02:21 +0530

    featured courses done

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,7 +4,7 @@ import { Button } from "./ui/moving-border";
 
 function HeroSection() {
     return (
-        <div className="h-screen md:h-[40rem] w-screen rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
+        <div className="h-[100vh] w-screen rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
             <Spotlight className="-top-40 left-0 md:left-60 md:-top-20" fill="white" />
             <div className="p-4 relative z-10 w-full text-center">
                 <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">Master the art of music</h1>
